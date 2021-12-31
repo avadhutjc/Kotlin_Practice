6baabc23b999b11613e61b7eb39bf49f3ede2e70
Avadhutjc <Avadhutjc@gmail.com>
2021-12-31 23:32:40 +0530

    done

---
+++ src/avadhut/Just.java
@@ -0,0 +1,29 @@
+package avadhut;
+
+public class Just {
+
+    int a;
+    int b;
+
+    public void CoolSong() {
+        System.out.println("Done Cool");
+    }
+
+    protected int getA() {
+        return a;
+    }
+
+}
+
+//class Me extends Just {
+//    @Override
+//    public void CoolSong() {
+//        super.CoolSong();
+//    }
+//
+//    public static void main(String[] args) {
+//        Me me = new Me();
+//        me.CoolSong();
+//    }
+//
+//}
---
+++ src/chougule/You.java
@@ -0,0 +1,22 @@
+package chougule;
+
+import avadhut.Just;
+
+public class You extends Just {
+
+    @Override
+    protected int getA() {
+        return super.getA();
+    }
+
+    @Override
+    public void CoolSong() {
+        //super.CoolSong();
+        //System.out.println("after");
+    }
+
+    public static void main(String[] args) {
+//        Just you = new You();
+//        you.CoolSong();
+    }
+}
