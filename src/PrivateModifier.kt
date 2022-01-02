3dc095489f807bd9bc6297f72414486b8e2ca9f1
Avadhutjc <Avadhutjc@gmail.com>
2022-01-02 09:14:11 +0530

    done

---
+++ src/PrivateModifier.kt
@@ -0,0 +1,19 @@
+//Demo of private modifier/ access modifier
+/*
+Private Modifier
+If you are using the private modifier in the declaration then it will be visible inside
+that particular class or file containing the declaration.
+ */
+
+private const val noOfStudent = 1000
+
+// Visible just inside this file
+private class PrivateModifier {
+
+    private val newCount = noOfStudent
+    //noOfStudent variable can be access only inside class
+}
+
+//private val newCount2 = newCount
+//We cannot assign newCount variable outside particular block
+/// ERROR: newCount is not accessible outside the Student class
