1eca7afcd43dc36f0476ecb4b8c2033b3e732e86
Avadhutjc <Avadhutjc@gmail.com>
2022-01-09 10:11:47 +0530

    dddd

--- src/Person.kt
+++ src/Person.kt
@@ -9,7 +9,6 @@ class Person {
                     " ContactNumber: $contactNumber \n" +
                     " Address: $address"
         )
-
     fun performLetOperation() {
         val person = Person().let {
             return@let "The name of the Person is: ${it.name}"
