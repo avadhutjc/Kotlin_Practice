91638ddfabd88737a4a0e253ba2b964ec77a25ea
Avadhutjc <Avadhutjc@gmail.com>
2022-01-14 12:09:22 +0530

    completed

--- src/NullSafe.kt
+++ src/NullSafe.kt
@@ -1,11 +1,9 @@
-import kotlin.math.roundToInt
-
 class NullSafe {
-    var a = arrayListOf(1,2,3)
+    var a = arrayListOf(1, 2, 3)
 }
-
 
 fun main() {
     val name: String = "Lloyd"
     print(name)
+    NullSafe().a
 }
